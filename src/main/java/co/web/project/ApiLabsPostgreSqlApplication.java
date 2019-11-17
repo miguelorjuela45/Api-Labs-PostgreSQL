d62fe55791d6194f69c598657cbc14e5c0c067b6
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-11-17 15:11:53 -0500

    primer commit con archivos iniciales

---
+++ src/main/java/co/web/project/ApiLabsPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package co.web.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLabsPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLabsPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/web/project/ApiLabsPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package co.web.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLabsPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
